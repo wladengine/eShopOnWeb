08316b981c3910a867e898e93e5a7c6b55a699b5
Vladislav Chikhira <Vladislav_Chikhira@epam.com>
2024-10-28 19:50:14 +0300

    add function app

---
+++ src/OrderItemsReserver/OrderItemsReserver.cs
@@ -0,0 +1,69 @@
+using System.Net;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Shop.Functions;
+
+public class OrderItemsReserver
+{
+    private readonly ILogger _logger;
+    private readonly OrderItemsReserverStorageConfig _storageConfig;
+
+    public OrderItemsReserver(ILoggerFactory loggerFactory, IOptions<OrderItemsReserverStorageConfig> storageConfig)
+    {
+        _logger = loggerFactory.CreateLogger<OrderItemsReserver>();
+        _storageConfig = storageConfig.Value;
+    }
+
+    [Function("OrderItemsReserver")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "reserve-order-items")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var order = await JsonHelper.DeserializeRequestAsync<Order>(req);
+
+        string blobName = await DumpOrderToBlobStorage(order);
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+        response.WriteString($"Order data was successfully written to {blobName}!");
+
+        return response;
+    }
+
+    private async Task<string> DumpOrderToBlobStorage(Order order)
+    {
+        var orderData = new
+        {
+            OrderId = order.Id,
+            Items = order.OrderItems.Select(oi => new OrderDetails()
+            {
+                ItemId = oi.ItemOrdered.CatalogItemId,
+                Quantity = oi.Units,
+                Price = oi.UnitPrice,
+            })
+        };
+
+        BlobServiceClient blobServiceClient = new BlobServiceClient(_storageConfig.ConnectionString);
+
+        // Get the container (folder) the file will be saved in
+        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_storageConfig.FileContainerName);
+
+        // Get the Blob Client used to interact with (including create) the blob
+        string blobName = $"{order.Id}.json";
+        BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+        // Upload the blob
+        using var fileStream = new MemoryStream();
+        await JsonHelper.SerializeAsync(fileStream, orderData);
+        await blobClient.UploadAsync(fileStream);
+
+        return blobName;
+    }
+}
---
+++ src/OrderItemsReserver/OrderItemsReserverStorageConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop.Functions;
+
+public class OrderItemsReserverStorageConfig
+{
+    public string ConnectionString { get; set; }
+    public string FileContainerName { get; set; }
+}
